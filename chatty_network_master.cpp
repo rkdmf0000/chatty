e6a8340cff97aa81ccc6cdbe98402c4810ea8cb5
Taxi_tabby <rkdmf0000@gmail.com>
2021-04-21 00:14:49 +0900

    sets

---
+++ chatty_network_master.cpp
@@ -0,0 +1,5 @@
+//
+// Created by my_fl on 2021-04-21.
+//아직 암것도 안함ㅋㅋ2
+
+#include "chatty_network_master.h"
---
+++ chatty_network_master.h
@@ -0,0 +1,14 @@
+//
+// Created by my_fl on 2021-04-21.
+//아직 암것도 안함ㅋㅋ
+
+#ifndef CHATTY_CHATTY_NETWORK_MASTER_H
+#define CHATTY_CHATTY_NETWORK_MASTER_H
+
+
+class chatty_network_master {
+
+};
+
+
+#endif //CHATTY_CHATTY_NETWORK_MASTER_H
